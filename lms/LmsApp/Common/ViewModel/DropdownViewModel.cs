86708139efb9ed6317d034509288ef3e5606da69
mahmud-777 <mahmud33k@gmail.com>
2018-04-23 20:25:19 +0600

    add BaseRequestModel

---
+++ lms/LmsApp/Common/ViewModel/DropdownViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.ViewModel
+{
+    class DropdownViewModel
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+
+        public object Data { get; set; }
+    }
+}
