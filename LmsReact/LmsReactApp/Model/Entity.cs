053e6f4e7fafbcd1a23a1311727fb5ac40cc74d4
mahmud-777 <mahmud33k@gmail.com>
2018-04-25 20:38:14 +0600

    app Initialize

---
+++ LmsReact/LmsReactApp/Model/Entity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    [Serializable]
+   public class Entity
+    {
+        public string Id { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public string CreatedBy { get; set; }
+
+        public DateTime Modified { get; set; }
+
+        public string ModifiedBy { get; set; }
+
+        public bool IsActive { get; set; } // can be toggled
+
+        public bool IsDeleted { get; set; } // can not be toggled
+    }
+}
