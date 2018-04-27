7a4372b6249241137679cc72c85a956c12208e76
mahmud-777 <mahmud33k@gmail.com>
2018-04-27 08:46:04 +0600

    add CourseViewModel

---
+++ LmsReact/LmsReactApp/ViewModel/CourseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ViewModel
+{
+   public class CourseViewModel: BaseViewModel<Course>
+    {
+        public CourseViewModel(Course course) : base(course)
+        {
+            Name = course.Name;
+            Topic = course.Topic;
+            IsFree = course.IsFree;
+        }
+
+        public string Name { get; set; }
+
+        public string Topic { get; set; }
+
+        public bool IsFree { get; set; }
+    }
+}
---
+++ LmsReact/LmsReactApp/ViewModel/DropdownViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ViewModel
+{
+   public class DropdownViewModel
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+
+        public object Data { get; set; }
+    }
+}
