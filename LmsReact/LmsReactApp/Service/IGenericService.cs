bb291098c6d1bef81e56ded268ae45674261a914
mahmud-777 <mahmud33k@gmail.com>
2018-04-27 09:24:53 +0600

    add GenericService

---
+++ LmsReact/LmsReactApp/Service/IGenericService.cs
@@ -0,0 +1,33 @@
+﻿using Model;
+using RequestModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModel;
+
+namespace Service
+{
+   public interface IGenericService<T, TRm, TVm> where T : Entity where TRm : BaseRequestModel<T> where TVm : BaseViewModel<T>
+    {
+        bool Add(T entity);
+
+        bool Delete(T entity);
+
+        bool Delete(string id);
+
+        bool Edit(T entity);
+
+        T GetById(string id);
+
+        Task<List<TVm>> GetAllAsync();
+
+        List<DropdownViewModel> GetDropdownList(TRm request);
+
+        Task<Tuple<List<DropdownViewModel>, int>> GetDropdownListAsync(TRm request);
+
+        Task<Tuple<List<TVm>, int>> SearchAsync(TRm request);
+
+        TVm GetDetail(string id);
+    }
+}
